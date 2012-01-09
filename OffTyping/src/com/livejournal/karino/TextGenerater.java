69c5e9a246db7a91e68c37a12221bfe6a8736bc4
karino <karino@karinovista>
2012-01-09 20:04:48 +0900

    Create textgenerater and it's tests.

---
+++ OffTyping/src/com/livejournal/karino/TextGenerater.java
@@ -0,0 +1,78 @@
+package com.livejournal.karino;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TextGenerater {
+	ArrayList<String> texts;
+	int currentIndex;
+	public TextGenerater()
+	{
+		initializeTexts();
+		shuffle();
+		
+		currentIndex = 0;
+	}
+	private void initializeTexts() {
+		texts = new ArrayList<String>();
+		String[] arr = new String[]{
+				"いまのところ",
+				"てきすとは",
+				"すべてはーどこーどでございます",
+				"ごぎょう", "はこべら", "ほとけのざ", "なずな", "すずな", "すずしろ", "せり",
+				"このごろは、", "だいぶあたたかくなってきた。", "ふりっくにゅうりょくのはやさは", "がんばってれんしゅうします。",
+				"けいたいでんわ", "りょうてでふりっく", "それなりにはやい", "あんどろいどのおかげ"
+		};
+		for(String st : arr)
+		{
+			texts.add(st);
+		}
+	}
+	private void shuffle() {
+		int size = texts.size();
+		Random rand = new Random();
+		for(int i = 0; i < size; i++)
+		{
+			int nextInt = rand.nextInt(size);
+			swap(i, nextInt);
+		}
+	}
+	
+	private void swap(int i, int j) {
+		String tmp = texts.get(i);
+		texts.set(i, texts.get(j));
+		texts.set(j, tmp);
+	}
+	public String getCurrent()
+	{
+		return getAt(currentIndex);
+	}
+	
+	public String getNext()
+	{
+		return getAt(currentIndex+1);
+	}
+	
+	public String getAfterNext()
+	{
+		return getAt(currentIndex+2);
+	}
+	
+	public String getAt(int i)
+	{
+		if(i < texts.size())
+			return texts.get(i);
+		return "";
+	}
+	
+	public void moveNext()
+	{
+		if(!isFinished())
+			currentIndex++;
+	}
+	
+	public boolean isFinished()
+	{
+		return currentIndex >= texts.size();
+	}
+}
---
+++ OffTypingTest/src/com/livejournal/karino/TextGeneraterTest.java
@@ -0,0 +1,53 @@
+package com.livejournal.karino;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextGeneraterTest {
+	TextGenerater tg = new TextGenerater();
+
+	@Test
+	public void testGetCurrent_MultipleCall_shouldReturnSame() {
+		assertEquals(tg.getCurrent(), tg.getCurrent());
+	}
+	
+	@Test
+	public void testGetCurrent_moveNext_makeGetCurrentDifferenct() {
+		String firstCur = tg.getCurrent();
+		tg.moveNext();
+		assertNotSame(firstCur, tg.getCurrent());
+	}
+	
+	@Test
+	public void testGetCurrent_getNext_shouldBeDifferent() {
+		assertNotSame(tg.getCurrent(), tg.getNext());
+	}	
+
+	@Test
+	public void testGetCurrent_getAfterNext_shouldBeDifferent() {
+		assertNotSame(tg.getCurrent(), tg.getAfterNext());
+	}	
+
+	@Test
+	public void testGetNext_getAfterNext_shouldBeDifferent() {
+		assertNotSame(tg.getNext(), tg.getAfterNext());
+	}	
+	
+	@Test
+	public void testIsFinished_initiallyShouldFalse() {
+		assertFalse(tg.isFinished());
+	}
+	
+	@Test
+	public void testIsFinished_shouldReturnTrueWhenAllTextDone() {
+		int size = tg.texts.size();
+		for(int i = 0; i < size; i++) {
+			assertFalse(tg.isFinished());
+			tg.moveNext();
+		}
+		assertTrue(tg.isFinished());
+	}	
+
+
+}
