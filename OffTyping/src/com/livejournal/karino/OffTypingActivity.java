814fe461cbcd1a97430dc6e43502c8d58f4907db
karino <karino@karinovista>
2012-01-09 19:14:12 +0900

    Basic layout and experiment done.

--- OffTyping/src/com/livejournal/karino/OffTypingActivity.java
+++ OffTyping/src/com/livejournal/karino/OffTypingActivity.java
@@ -2,6 +2,10 @@ package com.livejournal.karino;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class OffTypingActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,32 @@ public class OffTypingActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        EditText et = (EditText)findViewById(R.id.editTextInput);
+        et.addTextChangedListener(new TextWatcher(){
+
+			public void afterTextChanged(Editable arg0) {
+				String current = arg0.toString();
+		        findTV(R.id.textViewDebug).setText(current);
+		        if("いまはこれをにゅうりょく。".equals(current))
+		        	((EditText)findViewById(R.id.editTextInput)).setText("");
+			}
+
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+			}
+
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+			}});
+        
+        findTV(R.id.textViewCurrent).setText("いまはこれをにゅうりょく。");
+        findTV(R.id.textViewNext).setText("つぎはこれをにゅうりょく。");
+        findTV(R.id.textViewAfterNext).setText("そのつぎはこれをにゅうりょく。");
+        
     }
+
+	private TextView findTV(int id) {
+		return (TextView)findViewById(id);
+	}
 }
