cf62547e9ab9cba5f72fe75b0bec7705fa2b3eb4
karino <karino@karinovista>
2012-01-09 18:19:15 +0900

    Initial version. Do nothing yet.

---
+++ OffTyping/src/com/livejournal/karino/OffTypingActivity.java
@@ -0,0 +1,13 @@
+package com.livejournal.karino;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OffTypingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
